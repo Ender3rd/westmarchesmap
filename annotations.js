7c3527de66e3c4569b3106310cb6b917c80c7cd7
Ender3rd <ender3rd@gmail.com>
2017-11-12 23:50:05 -0500

    Add files via upload
    First pass at setting up a website for my game

---
+++ annotations.js
@@ -0,0 +1,18 @@
+window.annotations = {
+	"38.06": "Sosonapa",
+	"42.14": "BogGobApolis",
+	"43.09": "Throal",
+	"43.10": "Baurschwine Farm",
+	"43.13": "Borgan's Rest",
+	"44.07": "Iceclaw Moot",
+	"44.14": "Niall P'Tlomais",
+	"46.12": "Kaer Avon",
+	"38.10": "Alchemy Island",
+	"41.11": "The Granite Keep",
+	"42.06": "Blood Bee Hive",
+	"42.08": "Horror Spotted Here",
+	"42.09": "Crystal Kaer",
+	"42.14": "Cultist Kaer",
+	"46.06": "Harpy Tower",
+	"47.06": "Approximate location of Wolf Father's Library"
+};
