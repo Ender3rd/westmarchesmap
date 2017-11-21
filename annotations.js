72917afd0b5dce4c73e5761c7a6cc42f8f09b027
Ender3rd <ender3rd@gmail.com>
2017-11-21 15:50:36 -0500

    new quest, new location.

--- annotations.js
+++ annotations.js
@@ -14,5 +14,6 @@ window.annotations = {
 	"42.09": "Crystal Kaer",
 	"46.06": "Harpy Tower",
 	"47.06": "Approximate location of Wolf Father's Library",
-	"39.13": "Cultist of Raggok Kaer"
+	"39.13": "Cultist of Raggok Kaer",
+	"45:10": "L'Krusk's hideout"
 };
